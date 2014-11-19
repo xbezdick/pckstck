67d444418ac9a73c4fc60766cdc79e5210860d2d
Lukas Bezdicka <lbezdick@redhat.com>
2014-11-19 16:50:54 +0100

    Add multinode support

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export LIBVIRT_DEFAULT_URI="qemu:///system"
-
+export VM_TYPES="controller,compute1,compute2,network"
 . $(dirname $0)/include/tmppath.rc
 . $(dirname $0)/include/virtpwn.rc
+. $(dirname $0)/include/pckstck.rc
 
 # function logging wrapper expecting human readable function name and instance name as $1 and $2
 function RUN()
@@ -22,119 +23,43 @@ function RUN()
   return $ret
 }
 
-function install_repo_on_vm()
-{
-  NAME=${1}
-  RPM_REPO_URL=${2}
-  cd ${PCKSTCK_DIR}/${NAME}
-  IP=$(get_vm_ip ${NAME})
-  ssh "root@${IP}" "yum -y install ${RPM_REPO_URL}"
-}
-
-function update_vm()
-{
-  NAME=${1}
-  cd ${PCKSTCK_DIR}/${NAME}
-  IP=$(get_vm_ip ${NAME})
-  ssh "root@${IP}" 'yum clean all; yum -y update'
-}
-
-function selinux_permissive_vm()
-{
-  NAME=${1}
-  cd ${PCKSTCK_DIR}/${NAME}
-  IP=$(get_vm_ip ${NAME})
-  ssh "root@${IP}" 'setenforce 0; sed -i -e "s/enforcing/permissive/" /etc/sysconfig/selinux' && \
-  ssh "root@${IP}" 'yum -y install policycoreutils-python auditd' && \
-  ssh "root@${IP}" 'service auditd restart'
-}
-
-function setup_packstack()
-{
-  NAME=${1}
-  PACKSTACK_URI=${2}
-  PACKSTACK_BRANCH=${3}
-  OPM_URI="${4%*/openstack-puppet-modules.git}/"
-  OPM_BRANCH=${5}
-  echo ${OPM_URI}
-  cd ${PCKSTCK_DIR}/${NAME}
-  IP=$(get_vm_ip ${NAME})
-  ssh "root@${IP}" 'yum -y install git PyYAML python-setuptools' && \
-  ssh "root@${IP}" "git clone ${PACKSTACK_URI} packstack" && \
-  ssh "root@${IP}" "cd packstack; git checkout ${PACKSTACK_BRANCH}" && \
-  ssh "root@${IP}" "sed -i -e 's,^MODULES_REPO.*$,MODULES_REPO = ('\''${OPM_URI}'\'',' packstack/setup.py" && \
-  ssh "root@${IP}" "sed -i -e 's/^MODULES_BRANCH.*$/MODULES_BRANCH = '\''${OPM_BRANCH}'\''/' packstack/setup.py" && \
-  ssh "root@${IP}" "cd packstack; python setup.py install_puppet_modules" && \
-  ssh "root@${IP}" "cd packstack; python setup.py install"
-}
-
-function configure_packstack()
-{
-  NAME=${1}
-  cd ${PCKSTCK_DIR}/${NAME}
-  IP=$(get_vm_ip ${NAME})
-  ssh "root@${IP}" "packstack --gen-answer-file=/root/pckstck.conf"
-}
-
-function run_packstack_config()
-{
-  NAME=${1}
-  CONFIG=${2}
-  cd ${PCKSTCK_DIR}/${NAME}
-  IP=$(get_vm_ip ${NAME})
-  ssh "root@${IP}" "packstack -d --answer-file=${CONFIG}"
-}
-
-function run_allinone()
-{
-  NAME=${1}
-  SOURCE_VM=${2}
-  PACKSTACK_URI=${3}
-  PACKSTACK_BRANCH=${4}
-  OPM_URI=${5}
-  OPM_BRANCH=${6}
-  RPM_REPO_URL=${7}
-  if [ "${RPM_REPO_URL}" != "" ]
-  then
-    RUN install_repo_on_vm ${NAME} ${RPM_REPO_URL} || return 1
-  fi
-  RUN update_vm ${NAME} && \
-  RUN selinux_permissive_vm ${NAME} && \
-  RUN setup_packstack ${NAME} ${PACKSTACK_URI} ${PACKSTACK_BRANCH} ${OPM_URI} ${OPM_BRANCH} && \
-  RUN configure_packstack ${NAME} && \
-  RUN run_packstack_config ${NAME} /root/pckstck.conf | tee ${PCKSTCK_DIR}/${NAME}/packstack.log && \
-  RUN run_packstack_config ${NAME} /root/pckstck.conf | tee ${PCKSTCK_DIR}/${NAME}/packstack2.log
-
-  # ensure we have local stored logs
-  RUN collect_logs ${NAME}
-}
-
-function allinone()
+function delete_multinode_vms()
 {
   SOURCE_VMS=${1}
-  PACKSTACK_URI=${2}
-  PACKSTACK_BRANCH=${3}
-  OPM_URI=${4}
-  OPM_BRANCH=${5}
-  RPM_REPO_URL=${6}
   for vm in ${SOURCE_VMS//,/ }; do
-    RUN prepare_virtpwn_vm "allinone-${vm}" "${vm}" && \
-    RUN run_virtpwn_vm "allinone-${vm}"
+    for vm_type in ${VM_TYPES//,/ }; do
+      RUN stop_virtpwn_vm "${vm_type}-${vm}"
+      RUN drop_virtpwn_vm "${vm_type}-${vm}"
+    done
   done
+}
+
+function prepare_multinode_vms()
+{
+  SOURCE_VMS=${1}
   for vm in ${SOURCE_VMS//,/ }; do
-    run_allinone "allinone-${vm}" "${vm}" \
-		"${PACKSTACK_URI}" "${PACKSTACK_BRANCH}" \
-		"${OPM_URI}" "${OPM_BRANCH}" \
-		"${RPM_REPO_URL}" &
+    for vm_type in ${VM_TYPES//,/ }; do
+      RUN prepare_virtpwn_vm "${vm_type}-${vm}" "${vm}" && \
+      RUN run_virtpwn_vm "${vm_type}-${vm}"
+    done
   done
-  wait
-  if $KEEP_VMS;
-  then
-   exit 0
-  fi
+}
+
+function delete_allinone_vms()
+{
+  SOURCE_VMS=${1}
   for vm in ${SOURCE_VMS//,/ }; do
     RUN stop_virtpwn_vm "allinone-${vm}"
     RUN drop_virtpwn_vm "allinone-${vm}"
+  done
+}
+
+function prepare_allinone_vms()
+{
+  SOURCE_VMS=${1}
+  for vm in ${SOURCE_VMS//,/ }; do
+    RUN prepare_virtpwn_vm "allinone-${vm}" "${vm}" && \
+    RUN run_virtpwn_vm "allinone-${vm}"
   done
 }
 
@@ -154,6 +79,10 @@ function usage()
                                    Default: https://repos.fedorapeople.org/repos/openstack/openstack-juno/rdo-release-juno-1.noarch.rpm
     -n|--source-vms <vms>          Comma separated list of VMs to use for clonning.
                                    All tests run for each source VM.
+    -d|--deploy <allinone,multi>   Comma separated list of deployment types to use. Multi node deployment will do
+                                   Controller Network and 2*Compute. Default: allinone
+    -p|--packstack-options <o=v;.> Semicollon separated packstack config options which will be set to packstack
+                                   config file with sed.
 EOF
 }
 
@@ -162,8 +91,8 @@ function echoerr()
 cat <<< "$@" 1>&2
 }
 
-SHORTOPTS="hkb::g::m::o::r::n:"
-LONGOPTS="help,packstack-branch::,packstack-git::,opm-branch::,opm-git::,repo::,source-vms:,keep"
+SHORTOPTS="hkb::g::m::o::r::n:d::p:"
+LONGOPTS="help,packstack-branch::,packstack-git::,opm-branch::,opm-git::,repo::,source-vms:,keep,deploy::,packstack-options:"
 PROGNAME=${0##*/}
 
 ARGS=$(getopt -s bash --options $SHORTOPTS  \
@@ -176,10 +105,11 @@ PACKSTACK_GIT='https://github.com/stackforge/packstack.git'
 OPM_BRANCH='master'
 OPM_GIT='https://github.com/redhat-openstack/openstack-puppet-modules.git'
 KEEP_VMS=false
+DEPLOY='allinone'
 
 # options that can be empty
 REPO=""
-
+PACKSTACK_OPTIONS=""
 # required options
 VMS=""
 
@@ -236,6 +166,19 @@ while true; do
     -k|--keep)
       export KEEP_VMS=true
       ;;
+    -d|--deploy)
+      case "${2}" in
+        ""|-*)
+          DEPLOY='allinone'
+          echoerr "No deployment selected. Using ${REPO}!" 
+          ;;
+        *) DEPLOY="${2}" ; shift ;;
+      esac
+      ;;
+    -p|--packstack-options)
+      PACKSTACK_OPTIONS="${2}"
+      shift
+      ;;
     -n|--source-vms)
       VMS="${2}"
       shift
@@ -257,6 +200,28 @@ then
   echoerr "No source VMs were specified, see README about preparing VMs"
   exit 1
 else
-allinone "${VMS}" "${PACKSTACK_GIT}" "${PACKSTACK_BRANCH}" "${OPM_GIT}" "${OPM_BRANCH}" "${REPO}"
+  ALLINONE=false
+  MULTI=false
+  for deployment in ${DEPLOY//,/ }; do
+    if [ "${deployment}" == "allinone" ]; then
+      ALLINONE=true
+    elif [ "${deployment}" == "multi" ]; then
+      MULTI=true
+    else
+      echoerr "No such deployment ${deployment} possible."
+      exit 1
+    fi
+  done
+  if $ALLINONE; then prepare_allinone_vms "${VMS}" && run_allinone "${VMS}" "${PACKSTACK_GIT}" "${PACKSTACK_BRANCH}" "${OPM_GIT}" "${OPM_BRANCH}" "${REPO}" "%{PACKSTACK_OPTIONS}" & fi
+  if $MULTI; then prepare_multinode_vms "${VMS}" && run_multinode "${VMS}" "${PACKSTACK_GIT}" "${PACKSTACK_BRANCH}" "${OPM_GIT}" "${OPM_BRANCH}" "${REPO}" "${PACKSTACK_OPTIONS}" & fi
+  wait
+  if $KEEP_VMS;
+  then
+   exit 0
+  fi
+  for vm in ${SOURCE_VMS//,/ }; do
+    RUN stop_virtpwn_vm "allinone-${vm}"
+    RUN drop_virtpwn_vm "allinone-${vm}"
+  done
 fi
 
